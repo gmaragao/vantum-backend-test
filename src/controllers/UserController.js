1735a61cb3bb176ee9561f7507cef8babcb7f6a3
Gabriel Aragão <gabriel.aragao.comp@gmail.com>
2019-08-14 19:00:03 -0300

    Adding user controller

---
+++ src/controllers/UserController.js
@@ -0,0 +1,32 @@
+'use strict';
+const User = require('../models/User');
+module.exports = {
+  async index(req, res, next) {
+    //INDEX
+  },
+  async store(req, res, next) {
+    const { name, email, phoneNumber, address } = req.body;
+    const newUser = new User({
+      name,
+      email,
+      phoneNumber,
+      address
+    });
+    await newUser
+      .save()
+      .then(newUser => {
+        res.status(200);
+        res.send(newUser);
+      })
+      .catch(err => {
+        res.status(err.status);
+        res.send(err);
+      });
+  },
+  async show(req, res, next) {
+    //SHOW
+  },
+  async delete(req, res, next) {
+    //DELETE
+  }
+};
