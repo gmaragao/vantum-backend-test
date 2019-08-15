88edf0f220cf31884ec4b6f0809778704def15b8
Gabriel Aragão <gabriel.aragao.comp@gmail.com>
2019-08-14 21:03:49 -0300

    Showing and creating user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,25 +6,49 @@ module.exports = {
   },
   async store(req, res, next) {
     const { name, email, phoneNumber, address } = req.body;
-    const newUser = new User({
-      name,
-      email,
-      phoneNumber,
-      address
-    });
-    await newUser
-      .save()
-      .then(newUser => {
-        res.status(200);
-        res.send(newUser);
+    await User.findOne({ email })
+      .then(async user => {
+        console.log(user);
+        if (!user) {
+          const newUser = new User({
+            name,
+            email,
+            phoneNumber,
+            address
+          });
+          await newUser
+            .save()
+            .then(newUser => {
+              res.status(200);
+              res.send(newUser);
+            })
+            .catch(err => {
+              res.status(400);
+              res.send(err);
+            });
+        } else {
+          res.status(400);
+          res.send({
+            error: `Usuário com email ${email} já cadastrado`
+          });
+        }
       })
       .catch(err => {
-        res.status(err.status);
+        res.status(400);
         res.send(err);
       });
   },
   async show(req, res, next) {
-    //SHOW
+    const { id } = req.params;
+    const user = await User.findById(id).catch(err =>
+      res.send({ message: 'Id incorreto' })
+    );
+    if (!user) {
+      res.status(400);
+      res.send({ message: 'Usuário não encontrado' });
+    }
+    res.status(200);
+    res.send(user);
   },
   async delete(req, res, next) {
     //DELETE
